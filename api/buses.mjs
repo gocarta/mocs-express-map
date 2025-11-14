e46b45d4ea17c65a5eb79d753d3916ae58422b55
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2025-11-14 08:03:37 -0500

    updated

--- api/buses.mjs
+++ api/buses.mjs
@@ -2,12 +2,25 @@ const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const { DynamoDBDocumentClient, QueryCommand } = require("@aws-sdk/lib-dynamodb");
 
 const TABLE_NAME = "TaipLocationData-dev";
-const VEHICLE_IDS = ["0135", "0143", "0754", "0756", "0757"];
+const VEHICLE_IDS = ["0135", "0143", "0144", "0754", "0756", "0757"];
 
 function clean_number(n) {
     if (n.startsWith("+")) n = n.replace("+", "");
     n = n.replace(/(\d{4})$/, ".$1");
     return Number(n);
+}
+
+async function getvehicles(rt) {
+    const results = [];
+    const url = "https://bustracker.gocarta.org/bustime/api/v3/getvehicles";
+    const searchParams = new URLSearchParams();
+    searchParams.append("format", "json");
+    searchParams.append("key", process.env.CLEVER_BUS_TIME_API_KEY)
+    searchParams.append("rt", rt);
+    const response = await fetch(url + "?" + searchParams.toString());
+    const data = await response.json();
+    console.log("data:", data);
+    return data['bustime-response']['vehicle'];
 }
 
 export const handler = async (event, context) => {
@@ -30,14 +43,40 @@ export const handler = async (event, context) => {
 
     const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);
 
+    const seen_vehicle_ids = new Set();
+
     // scan dynamodb for each vehicle ID
     const geojson = {
         type: "FeatureCollection",
         features: []
     };
 
+    const vehicles = await getvehicles(14);
+    console.log(vehicles);
+
+    vehicles.forEach(vehicle => {
+        geojson.features.push({
+            type: "Feature",
+            properties: {
+                vehicleId: vehicle.vid,
+                latitude: vehicle.lat,
+                longitude: vehicle.lon,
+                timestamp: vehicle.tmstmp
+            },
+            geometry: {
+                type: "Point",
+                coordinates: [vehicle.lon, vehicle.lat]
+            }
+        });
+        seen_vehicle_ids.add(Number(vehicle.vid));
+    });
+
     for (let i = 0; i < VEHICLE_IDS.length; i++) {
         const vehicleId = VEHICLE_IDS[i];
+
+        if (seen_vehicle_ids.has(Number(vehicleId))) {
+            continue;
+        }
 
         const queryParams = {
             TableName: TABLE_NAME,
